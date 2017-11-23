04e4a17b056c52db0359df7765c44cd5d882a836
Colby Gutierrez-Kraybill <colbygk@media.mit.edu>
2017-11-23 01:57:24 -0500

    final form

--- lambda.js
+++ lambda.js
@@ -7,15 +7,13 @@ const Promise = require('es6-promise').Promise;
 const daysToWarn = parseInt(process.env.DAYS_TO_WARN || '30', 10);
 
 exports.handler = (event, context, callback) => {
-    log.info('event:',JSON.stringify(event, null, 2));
-    log.info('remaining time::',context.getRemainingTimeInMillis());
 
     var response = {};
     var certInfo = {};
     
     let checks = event.urlList.map( (target) => {
         if (target.length > 0) {
-            log.info('promising target:',target);
+            log.info('target:',target);
             return new Promise( (resolve) => {
                 certificate.getCertificate(target, false, (cert) => {
                     certInfo = api.certificateCheck(cert, daysToWarn);
